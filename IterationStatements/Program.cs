915ea56c50d97ee1a50dad923d8bb6eba1a136a4
hassan8118 <mujtabahassan1234567@gmail.com>
2023-04-14 14:37:04 -0400

    IterationsProject done

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -74,9 +74,9 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)
             // AND as long as i is greater than or equal to 0
             // Decrement i by 1
-            for (int i = 199; i <= numbers.Count && i >= 0; i--)
+            for (int i = 200; i <= numbers.Count && i >= 0; i--)
                 {
-                   Console.WriteLine(i);
+                Console.WriteLine(numbers[i]);
                  }
 
             //start for loop here
